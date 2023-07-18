fdc179306cc0614823306700a5ccc41b2e1fe999
wendellsantos2 <wemdellcos@gmail.com>
2023-07-18 16:42:59 -0300

    feat  and config: Atualização do projeto

---
+++ Profiles/UsuarioProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+ 
+using UsuarioApi.Models;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuarioApi.Profiles
+{
+    public class UsuarioProfile : Profile 
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<CreateUsuarioDto, Usuario>();
+        }
+    }
+}
